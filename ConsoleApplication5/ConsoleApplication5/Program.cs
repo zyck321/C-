daca72f8458d79a2a63ada58ea0e7325f7c8be67
zyck321 <48078376+zyck321@users.noreply.github.com>
2019-02-28 13:32:55 +0900

    1
    1

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int a = 0;
+            int b = 0;
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
