d455ce49a66c10937e192adea22fc24a81f7073d
zyck321 <48078376+zyck321@users.noreply.github.com>
2019-03-21 00:02:29 +0900

    1

---
+++ 作业4/1/ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        public static void CallToChildThread()
+        {
+            Console.WriteLine("BIBIBIBIBIBIBIBI!!!!!!!!!");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            int a=Convert.ToInt32(Console.ReadLine());
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            Thread childth = new Thread(childref);
+            Thread.Sleep(a);
+            childth.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
